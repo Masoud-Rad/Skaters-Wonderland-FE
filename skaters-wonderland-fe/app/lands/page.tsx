6e888bdfc63dfa9726211259bb6429473ae63c16
Masoud <masoud.rad.uk@gmail.com>
2024-01-03 23:52:57 +0000

    fetch Lands data and display list of skate lands on LandsPage

---
+++ skaters-wonderland-fe/app/lands/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Link from 'next/link'
+//**** landsType
+interface LandSample {
+    land_id: number;
+    landname: string;
+    city: string;
+    country: string;
+    postcode: string;
+    description: string;
+    created_at: Date;
+    vote: number;
+    safety_rating_total: number;
+    safety_rating_count: number;
+    safety_rating_ave: number;
+    suitability_rating_total: number;
+    suitability_rating_count: number;
+    suitability_rating_ave: number;
+    cost: string;
+    is_public: boolean;
+    has_rink: boolean;
+    suitabile_for: string;
+    land_img_url: string;
+    username: string;
+  }
+
+interface LandResult {
+    lands: LandSample[];
+}
+const LandsPage =  async () => {
+
+   const res: Response = await fetch(`https://skaters-wonderland-be.onrender.com/api/lands`)
+   const {lands}: LandResult = await res.json();
+   
+  return (
+    <>
+    <h1>Skater Wonderlands</h1>
+    <ul>
+        {
+            lands.map(land=><li key={land.land_id}><Link href=''>{land.landname}</Link></li>)
+        }
+    </ul>
+    
+    </>
+  )
+}
+
+export default LandsPage
--- skaters-wonderland-fe/app/page.tsx
+++ skaters-wonderland-fe/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1> Hello Skaters! </h1>
+     <Link href='/lands'>See all Skater Wonderlands</Link>
     </main>
   )
 }
