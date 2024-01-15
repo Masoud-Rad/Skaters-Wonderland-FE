7d30093c213f44a076921ad15d7cade11322401b
Masoud <masoud.rad.uk@gmail.com>
2024-01-15 18:49:39 +0000

    working on home page...

---
+++ skaters-wonderland-fe/app/loading.tsx
@@ -0,0 +1,11 @@
+
+const loading = () => {
+  return (
+    <>
+      <h1 className="items-center text-10">loading....</h1>
+      <div className="items-center text-10">Please wait!</div>
+   </>
+  )
+}
+
+export default loading
--- skaters-wonderland-fe/app/page.tsx
+++ skaters-wonderland-fe/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className="text-3xl font-bold underline"> Hello Skaters! </h1>
-     <Link href='/lands'>See all Skater Wonderlands</Link>
-     <Link href='/users'>See all the users</Link>
-     <h2>Happy skating</h2>
+      <h1 className="text-3xl font-bold underline"> Hello Skaters! </h1>
+      <Link href="/lands">See all Skater Wonderlands</Link>
+      <Link href="/users">See all the users</Link>
+      <h2>Happy skating</h2>
     </main>
-  )
+  );
 }
+ 
