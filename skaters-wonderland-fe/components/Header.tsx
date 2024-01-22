d19280b147ad32686c44b7ddf045f3daa03060b6
Masoud <masoud.rad.uk@gmail.com>
2024-01-22 17:35:11 +0000

    some STYLE change

--- skaters-wonderland-fe/components/Header.tsx
+++ skaters-wonderland-fe/components/Header.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import SiteNav from './SiteNav'
-import { TbSkateboarding } from "react-icons/tb"; 
+
 
 const Header = () => {
   return (
     <>
-      <div className = "border-2 shadow-lg rounded-xl items-center mx-auto" >
-        <TbSkateboarding />
-        <div className='flex justify-between rounded-3xl bg-gray-50 p-9'>
+      
+        <div className='flex border-2 rounded-xl justify-between shadow-lg bg-gray-50 p-9'>
           <SiteNav />
           <button className='border-2 border-blue-600 rounded-2xl text-blue-600 shadow-sm p-1 px-2 m-2'>Log in</button>
         </div>
-      </div>
+      
     </>
   )
 }
