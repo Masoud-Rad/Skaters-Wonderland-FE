8881e15c0025d7cc3a7b6bdc27b68bd205b4cd5e
Masoud <masoud.rad.uk@gmail.com>
2024-02-15 20:30:19 +0000

    I created addLnd but I have server error

--- skaters-wonderland-fe/utils.ts
+++ skaters-wonderland-fe/utils.ts
@@ -4,6 +4,17 @@ const SwApi = axios.create({
   baseURL: 'https://skaters-wonderland-be.onrender.com/api'
 });
 
+//--------------------------------Types---------------------
+
+interface LandsPostBody{
+    landname: string,
+    city: string,
+    country: string,
+    postcode: string,
+    description: string,
+    land_img_url: string,
+    username: string,
+  }
 
 //--------------------------------lands---------------------
 
@@ -17,4 +28,16 @@ export const fetchLands = ()=>{
     .catch((error) =>{
       console.log(error);
     })
+  }
+
+  export const addNewLand = (postBody: LandsPostBody)=>{
+console.log("in utils, postBody:", postBody)
+    return  SwApi.post('/land', postBody)
+      .then((response)=> {
+        console.log("in utils, postedLand:", response)
+        return(response);
+      })
+      .catch((error) =>{
+        console.log(error.status);
+      })
   }
